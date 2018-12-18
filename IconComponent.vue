cd1974aa81ef9388b0c5e79805e8c3857a678be1
James Johnson <james.johnson@excellentingenuity.com>
2018-12-18 09:07:40 -0700

    added script and component

---
+++ IconComponent.vue
@@ -0,0 +1,17 @@
+<template>  
+    <span>
+        <div v-html="require(`SVG/${icon}.svg`)"></div>
+    </span>
+   
+</template> 
+<script>
+
+export default {
+
+    props: {
+        'icon': {
+            type: String
+        }
+    }
+}
+</script>
--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 let mix = require('laravel-mix');
 
 class SVGIcon {
+    
+    name() {
+        return 'svgicon'
+    }
+    
+    dependencies() {
+        return ['vue-svg-loader'];
+    }
+
+    webpackRules() {
+        return {
+            test: /\.svg$/,
+            loader: 'html-loader',
+            options: {
+                minimize: true
+            }
+        };
+    }
+
+    webpackConfig(webpackConfig) {
+        webpackConfig.resolve.alias.SVG = path.resolve(__dirname, this.configPath);
+        // TODO: add support for additional paths
+        //webpackConfig.resolve.alias.Zondicons = path.resolve(__dirname, '../svg/zondicons');
+    }
+
+    register(configPath = './public/resources/svg') {
+        this.configPath = configPath;
+    }
+
+    boot() {
+        //console.log('loading svgicon');
+        Mix.listen('configReady', function (config) {  
+            const rules = config.module.rules;
+            const targetRegex = /(\.(png|jpe?g|gif)$|^((?!font).)*\.svg$)/;
+            
+            for (let rule of rules) {
+                if (rule.test.toString() == targetRegex.toString()) {
+                rule.exclude = /\.svg$/;
+                break;
+                }
+            }
+        });
+    }
 
 }
 
