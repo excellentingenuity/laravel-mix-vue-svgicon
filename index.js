3520a13998e55d359223d55f55d6067a7a8d5235
James Johnson <james.johnson@excellentingenuity.com>
2020-04-30 12:57:39 -0600

    changing method of getting svgs

--- index.js
+++ index.js
@@ -8,41 +8,99 @@ class SVGIcon {
     }
     
     dependencies() {
-        
+        return ['img-loader', 'imagemin-svgo', 'raw-loader', 'fs', 'svg-vue'];
+    }
+
+    register(options) {
+        this.options = Object.assign({
+            svgPath: 'resources/svg',
+            extract: false,
+            svgoSettings: [
+                { removeTitle: true },
+                { removeViewBox: false },
+                { removeDimensions: true }
+            ]
+        }, options);
+
+        this.includePath = path.resolve(__dirname, process.cwd() + '/' + this.options.svgPath);
+    }
+
+    boot() {
+        Mix.listen('configReady', config => {
+            config.module.rules.map(r => {
+                if (this._isSvgRegExp(r.test) && ! this._isSvgVueRule(r)) {
+                    r.exclude = path.resolve(__dirname, process.cwd() + '/' + this.options.svgPath);
+                }
+            });
+        });
     }
 
     webpackRules() {
         return {
             test: /\.svg$/,
-            loader: 'svg-inline-loader'
-        };
+            include: [
+                this.includePath
+            ],
+            loaders: [
+                {
+                    loader: 'raw-loader'
+                },
+
+                {
+                    loader: 'img-loader',
+                    options: {
+                        plugins: [
+                            require('imagemin-svgo')({ plugins: this.options.svgoSettings })
+                        ]
+                    }
+                }
+            ]
+        }
     }
 
-    register(configPath = './../public/resources/svg') {
-        this.configPath = configPath;
-        let absPath = process.cwd() + '/' + this.configPath;
-        mix.webpackConfig({
-            resolve: {
-                alias: {
-                    'SVGPATH': path.resolve(__dirname, absPath)
-                }
-            }
-        })
-    }
+    webpackConfig(webpackConfig) {
+        let fs = require('fs');
 
-    boot() {
-        //console.log('loading svgicon');
-        Mix.listen('configReady', function (config) {  
-            const rules = config.module.rules;
-            const targetRegex = /(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/;
-            
-            for (let rule of rules) {
-                if (rule.test.toString() == targetRegex.toString()) {
-                rule.exclude = /\.svg$/;
-                break;
-                }
-            }
+        fs.mkdir(this.includePath, error => {
+            if (error && error.code === 'EEXIST') return null;
         });
+
+        webpackConfig.resolve.alias['svg-files-path'] = this.includePath;
+
+        if (this.options.extract) {
+            let svgAssetsObj = {
+                test: this.includePath,
+                name: '/js/svg',
+                chunks: 'all',
+                enforce: true
+            }
+
+            if (webpackConfig.optimization.hasOwnProperty('splitChunks')) {
+                webpackConfig.optimization.splitChunks.cacheGroups['svgAssets'] = svgAssetsObj;
+            } else {
+                webpackConfig.optimization = {
+                    splitChunks: {
+                        cacheGroups: {
+                            svgAssets: svgAssetsObj
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    _isSvgRegExp(pattern) {
+        let regExCheck = new RegExp(pattern);
+
+        return regExCheck.test('.svg') || regExCheck.test('font.svg');
+    }
+
+    _isSvgVueRule(rule) {
+        if (rule.hasOwnProperty('include')) {
+            return rule.include.includes(this.includePath);
+        }
+
+        return false;
     }
 
 }
